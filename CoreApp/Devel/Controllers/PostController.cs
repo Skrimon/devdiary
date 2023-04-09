7ab7f9ceea5325ad8c49d9ca34773418f4026413
Simon Kim <simonkim@microsoft.com>
2023-04-09 01:38:36 -0700

    init

---
+++ CoreApp/Devel/Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Devel.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"This is the welcome action method, my name is: {name}, id is {ID} ");
+        }
+    }
+}
---
+++ CoreApp/Devel/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Devel.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PostedDateTime { get; set; }
+
+        public string? Content { get; set; }
+    }
+}
